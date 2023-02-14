dec55e04d0683fc1c62c21daaf127f9422555c7e
Leticia <leticiaae@hotmail.com.br>
2023-02-14 01:23:51 -0300

    criando componente do menu inferior da tela

---
+++ components/BottomMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BottomMenu=()=> {
+    return (
+        <View style={{backgroundColor:'#788D6D'}}>
+            <Text>OI</Text>
+        </View>
+    )
+ 
+};
+
+export default BottomMenu;
