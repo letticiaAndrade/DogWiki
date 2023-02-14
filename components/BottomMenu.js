89948e09dbc6f96852c598644dd9463eb637d7d7
Leticia <leticiaae@hotmail.com.br>
2023-02-14 01:57:27 -0300

    alterando dimensões do menu inferior

--- components/BottomMenu.js
+++ components/BottomMenu.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 const BottomMenu=()=> {
     return (
-        <View style={{backgroundColor:'#788D6D', width: 415, height:186, marginTop:80}}>
+        <View style={{backgroundColor:'#788D6D', width: '100%', height:'20%',marginTop:150}}>
             
         </View>
     )
