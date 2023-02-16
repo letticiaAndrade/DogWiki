09cad6b9abe919df30b0605335fb01e8b057211b
Leticia <leticiaae@hotmail.com.br>
2023-02-16 11:10:07 -0300

    criando o card da lista com imagens

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,30 @@
 import React from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const Card=(props)=> {
+    return (
+        <View >
+            <Image
+             source={props.image}   
+            />
+            <Text >
+                {props.nome}
+            </Text>
+        </View>
+    )   
+};
+
+const style = StyleSheet.create({
+    text: {
+        color: "#333333",
+        fontSize: 16,
+    },
+    image:{
+        borderRadius: 15,
+        //width:
+        // height:
+        
+    }
+})
+
+export default Card;
